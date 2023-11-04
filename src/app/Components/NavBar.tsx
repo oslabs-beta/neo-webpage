c7695a8c0e0034a7e632b4dcb31a85842c99bbe6
Nitesh Sunku <nsunku99@gmail.com>
2023-11-03 23:50:37 -0700

    added NavBar link for the contacts/team members

--- src/app/Components/NavBar.tsx
+++ src/app/Components/NavBar.tsx
@@ -3,13 +3,13 @@ import Image from "next/image";
 export default function NavBar() {
 
   return (
-    <nav className="mx-auto my-5 w-[60vw]">
+    <nav className="mx-auto my-5 w-[65%]">
       <div className="flex flex-wrap items-center justify-between gap-x-10">
           <Image className='my-5' src="/Neo-White.png" width={220} height={220} priority={true} alt="Neo Logo" />
           <div className="flex items-center justify-between gap-x-8 my-auto text-2xl font-light text-white">
             <a href="#test1">Test 1</a>
+            <a href="#team">Team</a>
             <a href="#test2">Test 2</a>
-            <a href="#test3">Test 3</a>
           </div>
         </div>
     </nav>
