2a32e4b1d43c64d7209a29cd68d044f939464922
Nitesh Sunku <nsunku99@gmail.com>
2023-11-03 23:49:10 -0700

    trying out background gradients

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
-import NavBar from './NavBar';
-import Footer from './Footer';
+import NavBar from './Components/NavBar';
+import Footer from './Components/Footer';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${inter.className} bg-gradient-to-t from-indigo-800/25 via-black to-purple-800/25`}>
         <NavBar />
         {children}
         <Footer />
