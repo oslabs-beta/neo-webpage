499a36d64ee671207e84c41b8f700cdfa922eaa2
Nitesh Sunku <nsunku99@gmail.com>
2023-11-04 19:09:15 -0700

    added navbar for start guide element

--- src/app/Components/NavBar.tsx
+++ src/app/Components/NavBar.tsx
@@ -4,15 +4,14 @@ import Link from "next/link";
 export default function NavBar() {
 
   return (
-    <nav className="p-5 sticky top-0 z-9999 bg-[rgba(29,15,43,0.75)] border-b-2 border-black">
+    <nav className="p-5 sticky top-0 z-9999 bg-[rgba(29,15,43,0.75)] border-b-2 border-black font-uk">
       <div className="flex flex-wrap items-center justify-between gap-x-10">
         <div className="flex items-center justify-around gap-x-8">
           <Image className='mx-5' src="/Neo-White.png" width={125} height={125} priority={true} alt="Neo Logo" />
-          <a className="text-xl" href="#team">Team</a>
+          <a className="text-lg" href="#team">Team</a>
+          <a className="text-lg" href="#start">Start</a>
         </div>
         <div className="flex items-center justify-between gap-x-4">
-          <a href="#test1">Test 1</a>
-          <a href="#test2">Test 2</a>
           <Link href={'https://github.com/nsunku99/neo-extension'}>
             <Image src="/github-logo.png" width={30} height={30} alt="GitHub Logo with a link"></Image>
           </Link>
